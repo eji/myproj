aab22a39fbc4c4c625ad5d2a25103e06316f7428
Koji Ejiri <k.ejiri@gmail.com>
2016-04-14 17:39:35 -0700

    removed unused settings

--- setup.sh
+++ setup.sh
@@ -134,26 +134,6 @@ git_clone()
   fi
 }
 
-# ユーザー共通の設定
-
-## マルチユーザーでHomebrewを使うための設定
-grep "umask 0002" "/etc/profile" 2>&1 1>/dev/null
-if [ $? -ne 0 ] ; then
-  sudo sh -c "echo 'umask 0002' >> /etc/profile"
-fi
-grep "umask 0002" "/etc/zprofile" 2>&1 1>/dev/null
-if [ $? -ne 0 ] ; then
-  sudo sh -c "echo 'umask 0002' >> /etc/zprofile"
-fi
-umask 0002
-
-## 既にHomebrewのパッケージがインストールされている場合は、ディレクトリのパーミッションを変更する
-sudo chmod -R g+w /usr/local
-sudo chgrp -R staff /usr/local
-sudo chmod -R g+w /opt/homebrew-cask
-sudo chgrp -R staff /Library/Caches/Homebrew
-sudo chmod -R g+w /Library/Caches/Homebrew
-
 ## パッケージのインストール
 brew update
 brew upgrade
